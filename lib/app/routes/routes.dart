bbd77de8bbb0e9633beb1f999a9d355c5b885712
akshayxemo <akshaykrdas001@gmail.com>
2026-01-14 22:23:44 +0530

    unused import removed

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,5 +1,4 @@
 
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quote_vault/presentation/screens/auth/signin_screen.dart';
 import 'package:quote_vault/presentation/screens/home/home_screen.dart';
